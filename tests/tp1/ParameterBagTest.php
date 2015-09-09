3c3eab2674d623b50b501545b008cf87f7ea3a50
Bastien SOARES <bastien.soares95@gmail.com>
2015-09-09 12:00:22 +0200

    add Travis config

--- tests/tp1/ParameterBagTest.php
+++ tests/tp1/ParameterBagTest.php
@@ -10,7 +10,7 @@ class ParameterBagTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->bag = new ParameterBag(array('foo' => 'bar', 'baz' => 'zab', 'toto' => 'tata', 'int' => 1234));
+        $this->bag = new ParameterBag(array('foo' => 'bar', 'baz' => 'zab', 'toto' => 'tata', 'int' => '1234'));
     }
 
     public function testCount()
@@ -25,38 +25,56 @@ class ParameterBagTest extends \PHPUnit_Framework_TestCase
 
     public function testGetInt()
     {
-        $this->assertEquals(0 , $this->bag->getInt('foo'));
-        $this->assertEquals(1234 , $this->bag->getInt('int'));
-        $this->assertEquals(9 , $this->bag->getInt('inconnu', 9));
+        $this->assertEquals(0, $this->bag->getInt('foo'));
+        $this->assertEquals(1234, $this->bag->getInt('int'));
+        $this->assertEquals(9, $this->bag->getInt('inconnu', 9));
     }
 
     public function testSet()
     {
-        $this->assertEquals($this->bag->get('toto') , $this->bag->set('toto', 'tutu'));
+        $this->bag->set('toto', 'tutu');
+        $this->assertEquals('tutu', $this->bag->get('toto'));
     }
 
     public function testHas()
     {
-
+        $this->assertTrue($this->bag->has('foo'));
     }
 
     public function testRemove()
     {
-
+        $this->bag->remove('foo');
+        $this->assertFalse($this->bag->has('foo'));
     }
 
     public function testAll()
     {
-
+        $this->assertEquals(array('foo' => 'bar',
+                                  'baz' => 'zab',
+                                  'toto' => 'tata',
+                                  'int' => '1234'
+                                  ), 
+                            $this->bag->all());
     }
 
     public function testKeys()
     {
-
+        $this->assertEquals(array('foo',
+                                  'baz',
+                                  'toto',
+                                  'int'
+                                  ),
+                            $this->bag->keys());
     }
 
     public function testAdd()
     {
-
+        $this->assertEquals(array('foo' => 'bar',
+                                  'baz' => 'zab',
+                                  'toto' => 'tata',
+                                  'int' => '1234',
+                                  'test' => 'unitaire'
+                                  ), 
+                            $this->bag->add(array('test' => 'unitaire')));
     }
 }
